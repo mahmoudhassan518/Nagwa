1138c6bfe9e6a607bd1bb1c7315883efea67a602
Mahmoud <mah.hassan4@gmail.com>
2021-08-09 12:49:12 +0200

    migrate groovy to kts install required Libs

---
+++ app/build.gradle.kts
@@ -0,0 +1,125 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId = "com.mahmoud.nagwa"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
+        testOptions {
+            unitTests.isReturnDefaultValues = true
+        }
+    }
+
+    buildTypes {
+
+
+
+        getByName("debug") {
+            isDebuggable = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    kotlinOptions {
+        // We have to add the explicit cast before accessing the options itself.
+        val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+        options.jvmTarget = "1.8"
+    }
+
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Kotlin.stdlib.jdk7)
+
+    implementation(AndroidX.appCompat)
+    implementation(AndroidX.constraintLayout)
+    implementation(Google.android.material)
+
+    implementation(Libs.downloader)
+    implementation(Libs.loader)
+
+    //-------------------------------------------- lifecycle -------------------------------
+    implementation(AndroidX.Core.ktx)
+    implementation(AndroidX.fragmentKtx)
+    implementation(AndroidX.activityKtx)
+    implementation(AndroidX.Lifecycle.viewModelKtx)
+    implementation(AndroidX.Lifecycle.runtimeKtx)
+    implementation(AndroidX.Lifecycle.liveDataKtx)
+
+    // --------------------- Dagger2 -------------------------
+    implementation(Libs.dagger2)
+    kapt(Libs.daggerCompiler)
+
+    //--------------------- RX -------------------------
+    implementation(Libs.reactivexAndroid)
+    implementation(Libs.reactivexJava)
+    implementation(Libs.reactivexBinding)
+    implementation(Libs.reactivexAdapter)
+
+    //-------------------------------------------- Logging -------------------------------
+    implementation(JakeWharton.timber)
+
+
+    // -------------------------------------- Networking -------------------------------
+    implementation(Square.okHttp3.okHttp)
+    implementation(Square.okHttp3.loggingInterceptor)
+
+    implementation(Square.retrofit2.retrofit)
+    implementation(Square.retrofit2.converter.gson)
+
+    // -------------------------------------- Navigation -------------------------------
+    implementation(AndroidX.Navigation.fragmentKtx)
+    implementation(AndroidX.Navigation.uiKtx)
+
+
+
+
+    testImplementation (Libs.Test.junit)
+    testImplementation (Libs.Test.xCore)
+    androidTestImplementation (Libs.Test.xCore)
+
+    androidTestImplementation (Libs.Test.arch)
+    androidTestImplementation (Libs.Test.rule)
+    androidTestImplementation (Libs.Test.runner)
+
+
+    testImplementation (Libs.Test.xArch)
+    androidTestImplementation (Libs.Test.xArch)
+    androidTestImplementation (Libs.Test.extJunit)
+
+    testImplementation (Libs.Test.mockito)
+    androidTestImplementation (Libs.Test.mockitoAndroid)
+
+    testImplementation (Libs.Test.mockk)
+
+
+    androidTestImplementation (Libs.Test.kaspresso)
+    androidTestImplementation (Libs.Test.kotlintest)
+
+}
---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,43 @@
+object Libs {
+
+    const val downloader = "com.amitshekhar.android:android-networking:1.0.2"
+    const val loader = "com.github.d-max:spots-dialog:1.1@aar"
+
+    const val dagger2 = "com.google.dagger:dagger:_"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:_"
+
+
+    const val reactivexAndroid = "io.reactivex.rxjava2:rxandroid:_"
+    const val reactivexJava = "io.reactivex.rxjava2:rxjava:_"
+    const val reactivexBinding = "com.jakewharton.rxbinding2:rxbinding:_"
+    const val reactivexAdapter = "com.squareup.retrofit2:adapter-rxjava2:_"
+
+    object Test {
+
+
+        const val kaspresso = "com.kaspersky.android-components:kaspresso:1.2.1"
+        const val kotlintest = "io.kotlintest:kotlintest-assertions-json:3.4.2"
+
+        // androidx.test junit
+        const val junit = "junit:junit:_"
+        const val xCore = "androidx.test:core:_"
+        const val rule = "com.android.support.test:rules:_"
+        const val runner = "androidx.test:runner:_"
+
+        // Test helpers for LiveData
+        const val arch = "android.arch.core:core-testing:_"
+        const val xArch = "androidx.arch.core:core-testing:_"
+        const val extJunit = "androidx.test.ext:junit:_"
+
+        //mockito
+        const val mockito = "org.mockito:mockito-core:_"
+        const val mockitoAndroid = "org.mockito:mockito-android:_"
+
+        //mockk
+        const val mockk = "io.mockk:mockk:_"
+
+    }
+
+
+
+}
